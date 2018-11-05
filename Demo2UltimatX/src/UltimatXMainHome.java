8e266cead4864717a4e5d3814dc8b53fcbd56283
digambarPa <digambar.patankar@tcs.com>
2018-11-05 17:52:01 +0530

    gspeed feature

--- Demo2UltimatX/src/UltimatXMainHome.java
+++ Demo2UltimatX/src/UltimatXMainHome.java
@@ -4,6 +4,8 @@ public class UltimatXMainHome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("inside home of ultimatix - main app");
+Gspeed gspeed1=new Gspeed();
+gspeed1.doneSevice();
 	}
 
 }
