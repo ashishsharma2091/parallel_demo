3261444e5021495a6979739dd49fcc290c20886c
digambarPa <digambar.patankar@tcs.com>
2018-11-16 13:55:11 +0530

    addding ievolve file

---
+++ Demo2UltimatX/src/IEvolve.java
@@ -0,0 +1,9 @@
+
+public class IEvolve {
+	public void inSevice() {
+		System.out.println("inside iEvolve service.");
+	}
+	public void doneSevice() {
+		System.out.println("done iEvolve service.");
+	}
+}
