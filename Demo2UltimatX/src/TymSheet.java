ae7333404a907a280523d45f07a65bfdd840d1df
digambarPa <digambar.patankar@tcs.com>
2018-11-22 10:47:45 +0530

    adding timesheet feature

---
+++ Demo2UltimatX/src/TymSheet.java
@@ -0,0 +1,9 @@
+
+public class TymSheet {
+	public void inSevice() {
+		System.out.println("timsheet iEvolve service.");
+	}
+	public void doneSevice() {
+		System.out.println("done timsheet service.");
+	}
+}
--- Demo2UltimatX/src/UltimatXMainHome.java
+++ Demo2UltimatX/src/UltimatXMainHome.java
@@ -4,6 +4,7 @@ public class UltimatXMainHome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("inside home of ultimatix - main app");
+System.out.println("integrated with Timesheet");
 	}
 
 }
